5f3158fc59fe2e1cf6c8978ae16c8c01640b4254
Firmin B <stereogaming18@gmail.com>
2022-08-12 15:02:55 +0200

    logs

--- commands/search-game.js
+++ commands/search-game.js
@@ -8,10 +8,11 @@ module.exports = {
       option
         .setName("platform")
         .setDescription("Platform to search")
-        .setRequired(true).addChoices(
-			{ name: 'Nintendo 3DS', value: '3ds' },
-			{ name: 'Nintendo DS', value: 'nds' },
-		)
+        .setRequired(true)
+        .addChoices(
+          { name: "Nintendo 3DS", value: "3ds" },
+          { name: "Nintendo DS", value: "nds" }
+        )
     )
     .addStringOption((option) =>
       option
@@ -23,26 +24,26 @@ module.exports = {
   async execute(interaction, client) {
     await interaction.deferReply({ content: "loading...", ephemeral: true });
     let finded = false;
-	let url;
+    let url;
     const game = interaction.options.getString("game");
-	const platform = interaction.options.getString("platform");
+    const platform = interaction.options.getString("platform");
 
-	if (platform === '3ds') {
-		url = "https://raw.githubusercontent.com/ghost-land/ghost-land.github.io/main/ghosteshop.json"
-	}
-	else if (platform === 'nds') {
-		url = "https://raw.githubusercontent.com/ghost-land/ghost-land.github.io/main/ghosteshop-ds.json"
-	}
+    if (platform === "3ds") {
+      url =
+        "https://raw.githubusercontent.com/ghost-land/ghost-land.github.io/main/ghosteshop.json";
+    } else if (platform === "nds") {
+      url =
+        "https://raw.githubusercontent.com/ghost-land/ghost-land.github.io/main/ghosteshop-ds.json";
+    }
 
     client.axios
-      .get(
-        url
-      )
+      .get(url)
       .then((response) => {
         const data = response.data.storeContent;
         for (let i = 0; i < data.length; i++) {
           if (data[i].info.title.toLowerCase().includes(game.toLowerCase())) {
             finded = true;
+            console.log('[search-game] ' + data[i].info.title);
             const embed = new EmbedBuilder()
               .setTitle(data[i].info.title ? data[i].info.title : "")
               .setThumbnail(data[i].info.icon_url ? data[i].info.icon_url : "")
@@ -51,16 +52,24 @@ module.exports = {
                   ? data[i].info.description
                   : "No description"
               )
-			  .addFields({
-				name:"Author",
-				value:data[i].info.author ? data[i].info.author : "",
-			  },{
-				name: "Console",
-				value: data[i].info.console ? data[i].info.console : "Unknown",
-			  },{
-				name: "Category",
-				value: data[i].info.category ? data[i].info.category : "Unknown",
-			  })
+              .addFields(
+                {
+                  name: "Author",
+                  value: data[i].info.author ? data[i].info.author : "",
+                },
+                {
+                  name: "Console",
+                  value: data[i].info.console
+                    ? data[i].info.console
+                    : "Unknown",
+                },
+                {
+                  name: "Category",
+                  value: data[i].info.category
+                    ? data[i].info.category
+                    : "Unknown",
+                }
+              )
               .setFooter({ text: "Availbale on ghosteshop.com" });
             interaction.editReply({ embeds: [embed], ephemeral: true });
             break;
